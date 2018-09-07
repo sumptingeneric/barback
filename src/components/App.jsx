96e4113be2404ee44bc049feb690a1396fe95aac
Michael Banzon <mrnbanzon@gmail.com>
2018-09-07 16:01:47 -0700

    Modified App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Search from "./customer/search.jsx";
+import Orders from "./customer/orders.jsx";
+import Menu from "./customer/menu.jsx";
 class App extends React.Component {
   state = {
     test: ""
@@ -10,6 +12,8 @@ class App extends React.Component {
       <div>
         <h1>Title</h1>
         <Search />
+        <Orders />
+        <Menu />
       </div>
     );
   }
