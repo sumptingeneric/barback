bd34ba8e238656ffc008135a4790bbca1eedf041
jpdaprato <jpdaprato@gmail.com>
2018-09-07 19:30:43 -0500

    Populate database with data via the server

--- server/server.js
+++ server/server.js
@@ -1,24 +1,16 @@
 var express = require("express");
+const bodyParser = require("body-parser");
 var db = require("../database/database.js");
-// Middleware
-// var morgan = require("morgan");
-// var parser = require("body-parser");
-// Router
-// var router = require("./routes.js");
+
 var app = express();
-//module.exports.app = app;
-// Set what we are listening on.
-// app.set("port", 3000);
-// Logging and parsing
-// app.use(morgan("dev"));
-// app.use(parser.json());
-// Set up our routes
-// app.use("/classes", router);
-// Serve the client files
-// app.use(express.static(__dirname + "/../client"));
-// If we are being run directly, run the server.
-// if (!module.parent) {
-// app.listen(app.get("port"));
-// console.log("Listening on", app.get("port"));
-// }
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/customers", (req, res) => {
+  db.Customers.create({ name: "Duke" }).then(customer => {
+    res.send(customer);
+  });
+});
+
 app.listen(3000, () => console.log("barback listening on port 3000!"));
