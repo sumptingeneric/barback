e72ba4ba3706bda061181fda02bfa8d83e4248e0
jpdaprato <jpdaprato@gmail.com>
2018-09-10 16:19:19 -0500

    Insert mockMenuItems and mockCustomers in the database

---
+++ database/seed.js
@@ -0,0 +1,141 @@
+const db = require("./database.js");
+
+let mockMenuItems = [
+  {
+    name: "Annah Banana",
+    price: 22.5,
+    category: "cocktail",
+    imageUrl: "/images/cocktails/annahbanana.png",
+    description: ""
+  },
+  {
+    name: "Hailey's Commit",
+    price: 25.0,
+    category: "cocktail",
+    imageUrl: "/images/cocktails/haileyscommit.png",
+    description: ""
+  },
+  {
+    name: "Rockin' Robin",
+    price: 11.84,
+    category: "cocktail",
+    imageUrl: "/images/cocktails/rockinrobin.png",
+    description: ""
+  },
+  {
+    name: "Cody on the Rockies",
+    price: 11.85,
+    category: "cocktail",
+    imageUrl: "/images/cocktails/codyontherockies.png",
+    description: ""
+  },
+  {
+    name: "Erwin's Elixr",
+    price: 21.0,
+    category: "cocktail",
+    imageUrl: "/images/cocktails/annahbanana.png",
+    description: ""
+  },
+  {
+    name: "Backend Zin",
+    price: 14.0,
+    category: "wine",
+    imageUrl: "/images/wine/backendzin.png",
+    description: ""
+  },
+  {
+    name: "Git Workflow Merlot",
+    price: 11.0,
+    category: "wine",
+    imageUrl: "/images/wine/gitworkflowmerlot.png",
+    description: ""
+  },
+  {
+    name: "MySQL Table Red",
+    price: 9.0,
+    category: "wine",
+    imageUrl: "/images/wine/mysqltablered.png",
+    description: ""
+  },
+  {
+    name: "Hack Reactor Pale Ale",
+    price: 8.5,
+    category: "beer",
+    imageUrl: "/images/beer/hackreactorpaleale.png",
+    description: ""
+  },
+  {
+    name: "HRR33 Breakfast Stout",
+    price: 12.0,
+    category: "beer",
+    imageUrl: "/images/beer/hrr33breakfaststout.png",
+    description: ""
+  },
+  {
+    name: "Senior Phase Haze Doppelbock",
+    price: 12.5,
+    category: "beer",
+    imageUrl: "/images/beer/seniorphasehazedoppelbock.png",
+    description: ""
+  }
+];
+
+const mockCustomers = [
+  {
+    name: "Carlos"
+  },
+  {
+    name: "Fredrick"
+  },
+  {
+    name: "Michael"
+  },
+  {
+    name: "Duke"
+  },
+  {
+    name: "J.P."
+  }
+];
+
+const mockOrders = [
+  {
+    status: 'current' // 'pending' and 'complete'
+    customersId: 3  // Check table schema description in db for accuracy of field name 
+  },
+];
+
+const mockOrderDetails = [
+  {
+
+  }
+];
+
+const insertMockMenuItems = () => {
+  db.MenuItems.create(mockMenuItems).then(
+    console.log("Mock MenuItems added to the database.")
+  );
+};
+
+const insertMockCustomers = () => {
+  db.Customers.create(mockCustomers).then(
+    console.log("Mock Customers added to the database.")
+  );
+};
+
+const insertMockOrders = () => {
+  db.Orders.create(mockOrders).then(
+    console.log("Mock Orders added to the database.")
+  );
+};
+
+const insertMockOrderDetails = () => {
+  db.OrderDetails.create(mockOrderDetails).then(
+    console.log("Mock OrderDetails added to the database.")
+  );
+};
+
+insertMockMenuItems();
+insertMockCustomers();
+insertMockOrders();
+insertMockOrderDetails();
