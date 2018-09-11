fd7d4f48edb091401bc4355572378f39c52504c3
jpdaprato <jpdaprato@gmail.com>
2018-09-11 18:57:19 -0500

    Provide complete seed data for development

--- database/seed.js
+++ database/seed.js
@@ -6,77 +6,86 @@ let mockMenuItems = [
     price: 22.5,
     category: "cocktail",
     imageUrl: "/images/cocktails/annahbanana.png",
-    description: ""
+    description: "Go bananas with this sweet and tangy tropical delight."
   },
   {
     name: "Hailey's Commit",
     price: 25.0,
     category: "cocktail",
     imageUrl: "/images/cocktails/haileyscommit.png",
-    description: ""
+    description:
+      "Bold and sassy, this drink is made with with as much love as it is fire!"
   },
   {
     name: "Rockin' Robin",
     price: 11.84,
     category: "cocktail",
     imageUrl: "/images/cocktails/rockinrobin.png",
-    description: ""
+    description:
+      "Our take on the old-fashioned made with Korbel brandy and a splash of cranberry juice."
   },
   {
     name: "Cody on the Rockies",
     price: 11.85,
     category: "cocktail",
     imageUrl: "/images/cocktails/codyontherockies.png",
-    description: ""
+    description:
+      "The classic EMT cocktail made with Everclear, Mountain Dew and Tanqueray gin."
   },
   {
     name: "Erwin's Elixr",
     price: 21.0,
     category: "cocktail",
-    imageUrl: "/images/cocktails/annahbanana.png",
-    description: ""
+    imageUrl: "/images/cocktails/erwinselixr.png",
+    description: "Like a Manhattan but better."
   },
   {
     name: "Backend Zin",
     price: 14.0,
     category: "wine",
     imageUrl: "/images/wine/backendzin.png",
-    description: ""
+    description:
+      "This 1998 Zinfandel from Jose Rodrigues Winery merges banal oatmeal midtones with a sleep-inducing sloppy joe finish."
   },
   {
     name: "Git Workflow Merlot",
     price: 11.0,
     category: "wine",
     imageUrl: "/images/wine/gitworkflowmerlot.png",
-    description: ""
+    description:
+      "A conventional millet perfume and cryptic clam undertones are united in this 2000 Coastal Merlot from Skunk Meadows Vineyards."
   },
   {
     name: "MySQL Table Red",
     price: 9.0,
     category: "wine",
     imageUrl: "/images/wine/mysqltablered.png",
-    description: ""
+    description:
+      "Scopazzi Winery brings together impossible-to-detect halibut elements and a nutty sauerkraut flavor in their 2001 Table Red."
   },
   {
     name: "Hack Reactor Pale Ale",
     price: 8.5,
     category: "beer",
     imageUrl: "/images/beer/hackreactorpaleale.png",
-    description: ""
+    description:
+      "Pours a magnificent chestnut with a nine-inch head. Excellent lacing. Lovely boozy nose, accompanied by hops and lilac. Intense yeasty taste, with just a hint of split pea soup and grapefruit. Thick and chewy mouthfeel and slightly dry finish."
   },
   {
     name: "HRR33 Oatmeal Stout",
     price: 12.0,
     category: "beer",
     imageUrl: "/images/beer/hrr33breakfaststout.png",
-    description: ""
+    description:
+      "Pours a breathtaking black with a thick, rocky head. Thick lacing. Beautiful spicy nose, punctuated with papaya and bacon. Well-balanced buttery flavor, and I also get some vanilla and citrus. Silky smooth mouthfeel and pleasant finish."
   },
   {
     name: "Senior Phase Haze Doppelbock",
     price: 12.5,
     category: "beer",
     imageUrl: "/images/beer/seniorphasehazedoppelbock.png",
-    description: ""
+    description:
+      "Pours an opalescent mahogany with a two-finger head. Heavy lacing. Excellent medicinal scent, with notes of mango and pepper. Delicate catty flavor, with overtones of citrus and molasses. Silky smooth mouthfeel and light finish."
   }
 ];
 
