a4d8140e660e0138dd222df0680aa9d30d21ae96
jpdaprato <jpdaprato@gmail.com>
2018-09-12 17:19:43 -0500

    Change status name in seed.js from "in progress" to "current"; fetch currentOrder and pendingOrders from the database when the Queue component mounts

--- src/components/business/queue.jsx
+++ src/components/business/queue.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 import QueueItem from "./queueitem.jsx";
+import axios from "axios";
 
 class Queue extends React.Component {
+  state = {
+    currentOrder: {},
+    pendingOrders: {}
+  };
+
+  componentDidMount() {
+    this.getCurrentOrder();
+    this.getPendingOrders();
+  }
+
+  getCurrentOrder() {
+    axios.get("http://localhost:7337/api/orders/current").then(response => {
+      this.setState({
+        currentOrder: response.data
+      });
+      console.log('This is the current order: ', this.state.currentOrder);
+    });
+  }
+
+  getPendingOrders() {
+    axios.get("http://localhost:7337/api/orders/pending").then(response => {
+      this.setState({
+        pendingOrders: response.data
+      });
+      console.log('These are the pending orders: ', this.state.pendingOrders);
+    });
+  }
+
   render() {
     return (
       <div>
