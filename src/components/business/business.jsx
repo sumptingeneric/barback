d465058399240cd244a70a34b284bbbe9b67cbdb
jpdaprato <jpdaprato@gmail.com>
2018-09-12 16:40:07 -0500

    Render Queue view by default; render Queue or PreviousOrders view on button click in Business view

--- src/components/business/business.jsx
+++ src/components/business/business.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import Queue from "./queue.jsx";
-import PreviousOrder from "./prevorder.jsx";
+import PreviousOrders from "./prevorder.jsx";
 
 class Business extends React.Component {
   state = {
-    active: {},
-    queue: [],
-    complete: []
+    view: 'queue'
   };
+
+  changeView(option) {
+    this.setState({
+      view: option
+    });
+  }
+
+  renderView() {
+    const { view } = this.state;
+    if (view === 'queue') {
+      return <Queue />;
+    } else {
+      return <PreviousOrders />;
+    }
+  }
 
   render() {
     return (
       <div>
-        <button>Queue</button>
-        <button>Previous Orders</button>
-        <Queue />
-        <PreviousOrder />
+        <button onClick={() => this.changeView('queue')}>Queue</button>
+        <button onClick={() => this.changeView('prev')}>Previous Orders</button>
+        <div className="main">{this.renderView()}</div>
       </div>
     );
   }
