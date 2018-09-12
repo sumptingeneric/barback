c54a54b1da4d751529da21a6d4b55577620526b0
jpdaprato <jpdaprato@gmail.com>
2018-09-12 17:02:53 -0500

    Allow for cross-origin requests from frontend to backend server

--- server/server.js
+++ server/server.js
@@ -16,6 +16,15 @@ if (process.env.NODE_ENV === "production") {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //Controllers
 
 //CUSTOMERS COLLECTION
