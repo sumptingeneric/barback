18e2e216b848be9d3113861d3c53c7122069c7e1
Charlie Astrada <carlosastrada@gmail.com>
2018-09-13 17:58:47 -0300

    Add styled components to App, orders, index (for alignment). Add Header component to encapsulate header components in the app.

--- src/components/customer/orders.jsx
+++ src/components/customer/orders.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 // test data... will delete after figuring out the props passed down.
 let dummyCustomerOrderPayload = [
@@ -28,7 +29,7 @@ let dummyCustomerOrderPayload = [
     items: [
       {
         item: "Leffe",
-        image_url: "/images/cocktails/leffe.png",
+        image_url: "/images/beers/leffe.png",
         quantity: 2
       },
       {
@@ -46,11 +47,30 @@ let dummyCustomerOrderPayload = [
   }
 ];
 
+
+//Styled Components
+const OrdersContainer = styled.div`
+  display: flex; 
+  flex-direction: row-reverse; 
+  justify-content: space-between; 
+  margin-top: 20px; 
+`;
+
+const Image = styled.img`
+  height: 50px; 
+`;
+
+const FlexContainerCentered = styled.div`
+  display: flex; 
+  align-items: center;
+  justify-content: center;
+`;
+
 // list of orders component
 // NOTE: we'll have to pass down the list of orders from the order component
 var OrderList = props => {
   return (
-    <div className="order-list">
+    <div>
       {dummyCustomerOrderPayload.map(order => {
         if (order.status === props.status) {
           return order.items.map((drink, index) => {
@@ -66,6 +86,7 @@ var OrderList = props => {
 var OrderItem = props => {
   return (
     <div className="order-item">
+      <Image alt="drink" src={props.drink.image_url} />
       <span>{props.drink.item}</span>
       <span>{props.drink.quantity}</span>
     </div>
@@ -74,17 +95,21 @@ var OrderItem = props => {
 
 var Orders = props => {
   return (
-    <div className="orders">
+    <section>
       <h1>Orders</h1>
-      <div className="orders-container">
-        <h4>In Progress</h4>
+      <OrdersContainer>
+        <FlexContainerCentered>
+          <h4>In Progress</h4>
+        </FlexContainerCentered>
         <OrderList status="in progress" orders={props.orders} />
-      </div>
-      <div className="orders-container">
-        <h4>Pending</h4>
+      </OrdersContainer>
+      <OrdersContainer>
+        <FlexContainerCentered>
+          <h4>Pending</h4>
+        </FlexContainerCentered>
         <OrderList status="pending" orders={props.orders} />
-      </div>
-    </div>
+      </OrdersContainer>
+    </section>
   );
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,18 @@ import { Router, Link } from "@reach/router";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import Business from "./components/business/business.jsx";
+import styled from 'styled-components';
+
+
+//Styled Components
+const Wrapper = styled.main`
+  display: grid;
+  grid-gap: 10px;
+  justify-items: center; 
+`;
 
 const Login = () => (
-  <div>
+  <Wrapper>
     <nav>
       <Link to="/customer">Customer</Link> <Link to="/business">Business</Link>
     </nav>
@@ -14,7 +23,7 @@ const Login = () => (
       <App path="/customer" />
       <Business path="/business" />
     </Router>
-  </div>
+  </Wrapper>
 );
 
 ReactDOM.render(<Login />, document.getElementById("root"));
