57f0540a3721d33de9c0e0ba6ecdad974ff5ee53
jpdaprato <jpdaprato@gmail.com>
2018-09-12 20:04:59 -0500

    Fetch orders with a status of "complete" once the PreviousOrders view component mounts

--- src/components/business/prevorder.jsx
+++ src/components/business/prevorder.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import PrevOrderItem from "./prevorderitem.jsx";
+import axios from "axios";
 
 class PreviousOrder extends React.Component {
+  state = {
+    previousOrders: []
+  };
+
+  componentDidMount() {
+    this.getPreviousOrders();
+  }
+
+  getPreviousOrders() {
+    axios.get("http://localhost:7337/api/orders/complete").then(response => {
+      this.setState({
+        previousOrders: response.data
+      });
+      console.log('These are the previous orders: ', this.state.previousOrders);
+    });
+  }
+  
   render() {
     return (
       <div>
         <h2>Previous Order</h2>
-        <PrevOrderItem />
+        <PrevOrderItem orders={this.state.previousOrders}/>
       </div>
     );
 
