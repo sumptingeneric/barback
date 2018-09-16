bebe0f0f181528499ddb3337343210a559676a87
Charlie Astrada <carlosastrada@gmail.com>
2018-09-16 20:07:48 -0300

    Add order functionality in order to control position of the items in carousel. Uses flexbox order property and is based on the DOM index of item.

--- src/components/customer/Carousel.jsx
+++ src/components/customer/Carousel.jsx
@@ -5,6 +5,23 @@ import Wrapper from './CarouselWrapper.jsx';
 import CarouselSlot from './CarouselSlot.jsx';
 
 class Carousel extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      position: 0
+    }
+  }
+
+  getOrder(itemIndex) {
+    const numItems = this.props.children.length || 1;
+
+    if (itemIndex - this.state.position < 0) {
+      return numItems - Math.abs(itemIndex - this.state.position)
+    }
+
+    return itemIndex - this.state.position;
+  }
+
   render() {
     const { title, children } = this.props
     return (
@@ -15,6 +32,7 @@ class Carousel extends React.Component {
             {children.map((child, index) => (
               <CarouselSlot
                 key={index}
+                order={this.getOrder(index)}
               >{child}
               </CarouselSlot>
             ))}
