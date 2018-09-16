bcc6ae9c349d017942de97f512d2d064b97682f6
Fredrick Lou <fredrick.lou@gmail.com>
2018-09-16 02:47:17 -0700

    Added refresh button to Business rather than interval (afraid might run into async problems)

--- src/components/business/queue.jsx
+++ src/components/business/queue.jsx
@@ -66,6 +66,7 @@ class Queue extends React.Component {
     return (
       <div>
         <h2>Queue</h2>
+        <button onClick={() => this.getPendingOrders()}>Refresh</button>
         <div className="current-order">
           <h3>This is the Current Order</h3>
           {this.renderCurrentOrder()}
