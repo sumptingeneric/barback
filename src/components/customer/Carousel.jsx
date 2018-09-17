2d12a3e8bcf930de5cbafff8aad99701d4fc9004
Charlie Astrada <carlosastrada@gmail.com>
2018-09-16 21:31:06 -0300

    Add mobile finger swiping functionality with throttling (500ms)

--- src/components/customer/Carousel.jsx
+++ src/components/customer/Carousel.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
 import CarouselContainer from './CarouselContainer.jsx';
-import Wrapper from './CarouselWrapper.jsx';
 import CarouselSlot from './CarouselSlot.jsx';
+import { throttle } from 'lodash';
+import Swipeable from 'react-swipeable'
+import Wrapper from './CarouselWrapper.jsx';
 
 class Carousel extends React.Component {
   constructor(props) {
@@ -13,6 +14,15 @@ class Carousel extends React.Component {
       sliding: false
     }
   }
+
+  //method to handle mobile swiping
+  handleSwipe = throttle(isNext => {
+    if (isNext) {
+      this.nextSlide()
+    } else {
+      this.prevSlide()
+    }
+  }, 500, { trailing: false })
 
   //Trigger animated state when we slide using transform in CarouselContainer
   triggerSliding = (direction, position) => {
@@ -27,6 +37,7 @@ class Carousel extends React.Component {
       })
     }, 50)
   }
+
 
   getOrder(itemIndex) {
     const numItems = this.props.children.length || 1;
@@ -53,23 +64,28 @@ class Carousel extends React.Component {
     return (
       <div>
         <h2>{title}</h2>
-        <Wrapper>
-          <CarouselContainer
-            sliding={this.state.sliding}
-            direction={this.state.direction}
-          >
-            {children.map((child, index) => (
-              <CarouselSlot
-                key={index}
-                order={this.getOrder(index)}
-              >{child}
-              </CarouselSlot>
-            ))}
-          </CarouselContainer>
-          <button onClick={() => this.prevSlide()}>Previous</button>
-          <button onClick={() => this.nextSlide()}>Next</button>
-        </Wrapper>
-      </div>
+        <Swipeable
+          onSwipingLeft={() => this.handleSwipe(true)}
+          onSwipingRight={() => this.handleSwipe()}
+        >
+          <Wrapper>
+            <CarouselContainer
+              sliding={this.state.sliding}
+              direction={this.state.direction}
+            >
+              {children.map((child, index) => (
+                <CarouselSlot
+                  key={index}
+                  order={this.getOrder(index)}
+                >{child}
+                </CarouselSlot>
+              ))}
+            </CarouselContainer>
+            <button onClick={() => this.prevSlide()}>Previous</button>
+            <button onClick={() => this.nextSlide()}>Next</button>
+          </Wrapper>
+        </Swipeable>
+      </div >
     )
   }
 }
