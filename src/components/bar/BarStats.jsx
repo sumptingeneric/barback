86cf696a165fdab2015a92125f7de7c02739466f
Joanne Kim <joanneskim831@gmail.com>
2018-09-20 23:16:30 -0700

    refactor front-end BarStats comp

--- src/components/bar/BarStats.jsx
+++ src/components/bar/BarStats.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import {ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
 
+const SelectWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+`;
 
 const data = [
   {name: "drink1", price: 13, quantity: 2, totalSales: 26, avgRating: 4},
@@ -13,7 +17,7 @@ const stats =[
   {name: "John", totalStars: "120", reviewCounts: 13}
 ]
 
-const StatsGraphs = props => {
+const StatsGraphs = () => {
   return (
     <ResponsiveContainer width={600} height={300}>
       <BarChart data={data} 
@@ -34,9 +38,49 @@ const StatsGraphs = props => {
 
 class BarStats extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {};
+    super(props)
+    this.state = {
+      view: "quantity"
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  renderView() {
+    const {view} = this.state;
+    console.log('inside renderView=', view);
+    if(view === "quantity") {
+      return <div>View 1</div>
+    } else {
+      return <div>View 2</div>
+    }
+  }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    console.log(this)
+    this.setState({
+      view: e.target.value
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <SelectWrapper>
+          <label htmlFor="sortBy">
+            <span>Sorted By:</span>{"\t"}
+            <select value={this.state.view} 
+              onBlur={this.handleChange}
+              onChange={this.handleChange}>
+              <option value="quantity">Quantity</option>
+              <option value="rating">Customer Rating</option>
+            </select>
+          </label>
+        </SelectWrapper>
+        <div>{this.renderView()}</div>
+      </div>
+    )
   }
 }
 
-export default StatsGraphs;
+export default BarStats;
